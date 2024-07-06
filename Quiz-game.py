c605b6a3e92e3d05702a384ee7f8ce1e4e6e27c2
tejeswarreddy991 <tejeswarreddypalakolanu2003@gmail.com>
2024-07-06 18:06:02 +0530

    Update Quiz-game.py

--- Quiz-game.py
+++ Quiz-game.py
@@ -45,8 +45,11 @@ print(f'You got {score} out of 4 questions correct.')
 
 # Optionally, you can provide a closing message based on the score
 if score == 4:
+    print('f you got {(score/4)*100} % ')
     print('Congratulations! You are a computer whiz!')
 elif score >= 2:
+    print('f you got {(score/4)*100} % ')
     print('Well done!')
 else:
+    print('f you got {(score/4)*100} % ')
     print('You might want to brush up on your computer knowledge.')
